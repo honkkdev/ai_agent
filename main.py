239fffd281578d85852782f36728b914b4608678
honkkdev <wilfred@cramar.co.uk>
2026-04-12 11:57:20 +0200

    2: main.py: added user prompts and AI now keeps history of messages.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from google import genai
 import os
+import argparse
+from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 
-
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 def main():
-    print("Hello from ai-agent!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     if api_key == None:
         raise RuntimeError("API key is required. Please set the GEMINI_API_KEY environment variable.")
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     if response.usage_metadata == None:
         raise RuntimeError("API request failed. No usage metadata returned.")
     else:
@@ -22,3 +27,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
