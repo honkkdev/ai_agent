490914424143d916e932dd19f53eb1f6a05cd2c0
honkkdev <wilfred@cramar.co.uk>
2026-04-12 12:13:57 +0200

    3: main.py: added --verbose flag.

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-# Now we can access `args.user_prompt`
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     if api_key == None:
         raise RuntimeError("API key is required. Please set the GEMINI_API_KEY environment variable.")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     if response.usage_metadata == None:
         raise RuntimeError("API request failed. No usage metadata returned.")
-    else:
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
