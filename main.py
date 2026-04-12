55ed7a903e7465702c7317c8a76fa52da81baab7
honkkdev <wilfred@cramar.co.uk>
2026-04-12 11:47:57 +0200

    1: began building AI agent in main.py. added hard-coded prompt.

---
+++ main.py
@@ -0,0 +1,24 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+
+def main():
+    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if api_key == None:
+        raise RuntimeError("API key is required. Please set the GEMINI_API_KEY environment variable.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if response.usage_metadata == None:
+        raise RuntimeError("API request failed. No usage metadata returned.")
+    else:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
